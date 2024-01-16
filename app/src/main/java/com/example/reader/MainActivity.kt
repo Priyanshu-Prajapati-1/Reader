faaa83d68c6066e690da3f96989bbea461c24364
Priyanshu-Prajapati-1 <priyanshu.lkouniv@gmail.com>
2024-01-16 19:47:29 +0530

    add SplashScreen

--- app/src/main/java/com/example/reader/MainActivity.kt
+++ app/src/main/java/com/example/reader/MainActivity.kt
@@ -4,13 +4,17 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.reader.navigation.ReaderNavigation
 import com.example.reader.ui.theme.ReaderTheme
 import com.google.firebase.firestore.FirebaseFirestore
 import dagger.hilt.android.AndroidEntryPoint
@@ -21,32 +25,31 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ReaderTheme {
-
-
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                ReaderApp()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ReaderApp(){
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            ReaderNavigation()
+        }
+
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ReaderTheme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/com/example/reader/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/reader/screens/home/HomeScreen.kt
@@ -1,8 +1,9 @@
 package com.example.reader.screens.home
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
 
 @Composable
-fun HomeScreen(){
+fun HomeScreen(navController: NavHostController) {
 
 }
